367e6c9486945e2524eec07c77328c27c036a493
coregas <fastes1@gmail.com>
2018-02-12 19:49:18 +0200

    abstract class for gateways

---
+++ src/Gateway/AbstractGateway.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Emotion\Gateway;
+
+use Illuminate\Database\Query\Builder;
+
+abstract class AbstractGateway
+{
+    /**
+     * @var Builder
+     */
+    private $table;
+
+    public function __construct(
+        Builder $table
+    ) {
+        $this->table = $table;
+    }
+
+    /**
+     * @param $set
+     * @return bool
+     */
+    public function insert($set)
+    {
+        return $this->table->insert($set);
+    }
+
+    /**
+     * @param $set
+     * @return int
+     */
+    public function update($set)
+    {
+        return $this->table->update($set);
+    }
+
+    /**
+     * @param $select
+     * @return Builder|static
+     */
+    public function select($select)
+    {
+        return $this->table->selectRaw($select);
+    }
+}
